cf239a5cbd65f5820e4c30f14d5efff12b2bcdc3
premekb <38076249+premekb@users.noreply.github.com>
2018-04-05 21:09:38 +0200

    Add files via upload

--- SpeedType.py
+++ SpeedType.py
@@ -6,7 +6,7 @@ WINDOWWIDTH = 640
 WINDOWHEIGHT = 480
 WORDS = []
 DIFFICULTY = 3
-DELAY = FPS * DIFFICULTY * 10000
+DELAY = FPS * DIFFICULTY
 putin = ""
 
 def main():
@@ -14,7 +14,7 @@ def main():
 
     wait_time = 0
     global BASICFONT, DISPLAYSURF, FPSCLOCK, slovo
-    
+
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     BASICFONT = pygame.font.Font('freesansbold.ttf', 20)
@@ -23,33 +23,29 @@ def main():
         slovo=file.readlines()
 
     get_rand_word()
-    
+
     while True:
         wait_time += 1
-        check_for_quit()
         check_for_input()
         if wait_time == DELAY:
             get_rand_word()
             wait_time = 0
             print(WORDS)
-        FPSCLOCK.tick()
-        
-        
-    
+        FPSCLOCK.tick(FPS)
+
+
+
 def terminate():
     pygame.quit()
     sys.exit()
 
-def check_for_quit():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            terminate()
-
 def check_for_input():
     global putin
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminate()
         if event.type == pygame.KEYDOWN:
-            if event.key != pygame.K_KP_ENTER:
+            if event.key != pygame.K_RETURN:
                 print(putin)
                 putin+=event.unicode
             else:
@@ -63,7 +59,7 @@ def get_rand_word():
         word =  word.rstrip("\n")
         WORDS.append(word)
         print(WORDS)
-    
-                
+
+
 if __name__ == '__main__':
     main()
