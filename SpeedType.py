b326499e76dd24ac4ccb2ed89e502babee3131a3
Přemek <premek.belka@seznam.cz>
2018-04-10 22:04:23 +0200

    backspace, limit znaku, refresh_score(), class word_on_screen
    Smazal jsem slova nad 30 znaku z words.txt a poresil jsem aby neslo ten pocet znaku prekrocit ve hre. Funguje uz backspace. Pridal jsem funkci na vykreslovani score a nejaky moc cool font xD. Do class word_on_screen jsem pridal metody na vykreslovani, pohyb a smazani slova.

--- SpeedType.py
+++ SpeedType.py
@@ -7,6 +7,7 @@ WORDS = []
 WORDS_ON_SCREEN = []
 DIFFICULTY = 3
 DELAY = FPS * DIFFICULTY
+WORD_MOVE_TIME = FPS * 1
 putin = ""
 SCORE = 0
 BGCOLOR = (0, 0, 0)
@@ -16,27 +17,38 @@ GREEN = (0, 255, 0)
 def main():
     pygame.init()
 
-    wait_time = 0
-    global BASICFONT, DISPLAYSURF, FPSCLOCK, slovo
+    wait_time_delay = 0
+    wait_time_word = 0
+    global BASICFONT, DISPLAYSURF, FPSCLOCK, slovo, char
 
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-    BASICFONT = pygame.font.Font('freesansbold.ttf', 20)
+    BASICFONT = pygame.font.Font('mocfancyfont.ttf', 20)
 
     with open("words.txt") as file:
         slovo=file.readlines()
 
     get_rand_word()
+    draw_ui()
 
     while True:
-        draw_ui()
-        wait_time += 1
+        refresh_score()
+        wait_time_delay += 1
+        wait_time_word += 1
         char = check_for_input()
         show_text(char)
-        if wait_time == DELAY:
+        if wait_time_delay == DELAY:
             get_rand_word()
-            wait_time = 0
+            wait_time_delay = 0
             print(WORDS)
+        for word in WORDS_ON_SCREEN:
+            if word.name not in WORDS:
+                word.word_remove()
+        if wait_time_word == WORD_MOVE_TIME:
+            for word in WORDS_ON_SCREEN:
+                word.word_move()
+                word.word_draw()
+            wait_time_word = 0
         FPSCLOCK.tick(FPS)
 
 def terminate():
@@ -50,7 +62,8 @@ def check_for_input():
             terminate()
         if event.type == pygame.KEYDOWN:
             if (event.key != pygame.K_RETURN) and (event.key != pygame.K_BACKSPACE):
-                putin+=event.unicode
+                if (len(putin)) <= 30:
+                    putin+=event.unicode
             elif event.key == pygame.K_RETURN and (putin in WORDS):
                 WORDS.remove(putin)
                 SCORE+=1
@@ -78,15 +91,18 @@ def show_text(char):
     text_surf = BASICFONT.render(putin,True,WHITE)
     text_rect = text_surf.get_rect()
     text_rect.bottomleft = (WINDOWWIDTH / 50,WINDOWHEIGHT / 10 * 9.8)
+    pygame.draw.rect(DISPLAYSURF, BGCOLOR, text_rect)
     DISPLAYSURF.blit(text_surf,text_rect)
 
     if char == "backspace":
-        DISPLAYSURF.fill(BGCOLOR)
-        #zmenit potom aby to zacernilo jenom ten text
+        remove_text = text_rect
+        remove_text[2] = WINDOWWIDTH / 1.5
+        pygame.draw.rect(DISPLAYSURF, BGCOLOR, remove_text)
+        DISPLAYSURF.blit(text_surf,text_rect)
 
     if char == "enter":
         remove_text = text_rect
-        remove_text[2] += WINDOWWIDTH / 1.5
+        remove_text[2] = WINDOWWIDTH / 1.5
         pygame.draw.rect(DISPLAYSURF, BGCOLOR, remove_text)
 
     pygame.display.update()
@@ -98,21 +114,42 @@ def draw_ui():
     text_surf=BASICFONT.render("SCORE:",True,WHITE)
     text_rect=text_surf.get_rect()
     text_rect.bottomleft = (WINDOWWIDTH / 1.4,WINDOWHEIGHT / 10 * 9.8)
-    text_surf2=BASICFONT.render(str(SCORE),True,WHITE)
-    text_rect2=text_surf.get_rect()
-    text_rect2.bottomleft = (WINDOWWIDTH / 1.18,WINDOWHEIGHT / 10 * 9.8)
     DISPLAYSURF.blit(text_surf,text_rect)
-    DISPLAYSURF.blit(text_surf2,text_rect2)
+
+def refresh_score():
+    text_surf=BASICFONT.render(str(SCORE),True,WHITE)
+    text_rect=text_surf.get_rect()
+    text_rect.bottomleft = (WINDOWWIDTH / 1.18,WINDOWHEIGHT / 10 * 9.8)
+    pygame.draw.rect(DISPLAYSURF, BGCOLOR, text_rect)
+    DISPLAYSURF.blit(text_surf,text_rect)
 
 class word_on_screen:
 
     def __init__(self, word):
+        global name, position
         self.name = word
-        self.position = [0,random.randint(0,WINDOWHEIGHT/10*9.0)]
+        self.position = [0,random.randint(20,WINDOWHEIGHT/10*9.0)]
 
-    def word_move():
-        self.position[0] += 10
+    def word_move(self):
+        self.position[0] += WINDOWWIDTH / 64
 
+    def word_draw(self):
+        text_surf = BASICFONT.render(self.name,True,WHITE)
+        text_rect = text_surf.get_rect()
+        delete_rect = text_surf.get_rect()
+        text_rect.bottomleft = tuple(self.position)
+        delete_rect.bottomleft = tuple(self.position)
+        delete_rect.left -= WINDOWWIDTH / 64
+        pygame.draw.rect(DISPLAYSURF, BGCOLOR, delete_rect)
+        DISPLAYSURF.blit(text_surf,text_rect)
+
+    def word_remove(self):
+        WORDS_ON_SCREEN.remove(self)
+        text_surf = BASICFONT.render(self.name,True,WHITE)
+        delete_rect = text_surf.get_rect()
+        delete_rect.bottomleft = tuple(self.position)
+        pygame.draw.rect(DISPLAYSURF, BGCOLOR, delete_rect)
 
 if __name__ == '__main__':
     main()
+#
