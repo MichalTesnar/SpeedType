62f5adbb3d788344fcaffde379f499ff1c7cb6bd
Přemek <premek.belka@seznam.cz>
2018-04-11 20:01:06 +0200

    Update

--- SpeedType.py
+++ SpeedType.py
@@ -7,12 +7,15 @@ WORDS = []
 WORDS_ON_SCREEN = []
 DIFFICULTY = 3
 DELAY = FPS * DIFFICULTY
-WORD_MOVE_TIME = FPS * 1
+WORD_MOVE_TIME = FPS * 0.1
 putin = ""
 SCORE = 0
 BGCOLOR = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+LIFECOUNT = 3
 
 def main():
     pygame.init()
@@ -29,10 +32,18 @@ def main():
         slovo=file.readlines()
 
     get_rand_word()
+    draw_start_screen("Start")
+
+    while True:
+        if check_for_input() == "enter":
+            break
+
+    DISPLAYSURF.fill(BGCOLOR)
     draw_ui()
 
     while True:
         refresh_score()
+        check_for_life()
         wait_time_delay += 1
         wait_time_word += 1
         char = check_for_input()
@@ -48,6 +59,7 @@ def main():
             for word in WORDS_ON_SCREEN:
                 word.word_move()
                 word.word_draw()
+                word.check_for_position()
             wait_time_word = 0
         FPSCLOCK.tick(FPS)
 
@@ -123,17 +135,69 @@ def refresh_score():
     pygame.draw.rect(DISPLAYSURF, BGCOLOR, text_rect)
     DISPLAYSURF.blit(text_surf,text_rect)
 
+def check_for_life():
+    global LIFECOUNT
+    position = [round(WINDOWWIDTH / 1.1),round(WINDOWHEIGHT / 10 * 9.6)]
+    pygame.draw.rect(DISPLAYSURF, BGCOLOR, (round(WINDOWWIDTH / 1.1),round(WINDOWHEIGHT / 10 * 9.6),WINDOWWIDTH,WINDOWHEIGHT),10)
+    for i in range (LIFECOUNT):
+        pygame.draw.circle(DISPLAYSURF,RED, position, 5)
+        position[0] += 15
+
+
+def game_over():
+    global WORDS, WORDS_ON_SCREEN
+    DISPLAYSURF.fill(BGCOLOR)
+    draw_start_screen("Game Over")
+    pygame.display.update()
+    WORDS = []
+    WORDS_ON_SCREEN = []
+    while True:
+        if check_for_input() == "enter":
+            main()
+
+def draw_start_screen(text):
+    global SCORE
+    text_surf = BASICFONT.render(text,True,GREEN)
+    text_rect = text_surf.get_rect()
+    text_rect.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
+    DISPLAYSURF.blit(text_surf,text_rect)
+
+    if text == "Start":
+        text_surf = BASICFONT.render("To procceed, press enter!",True,GREEN)
+        text_rect = text_surf.get_rect()
+        text_rect.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2+30)
+        DISPLAYSURF.blit(text_surf,text_rect)
+    else:
+        text_surf = BASICFONT.render(text,True,RED)
+        text_rect = text_surf.get_rect()
+        text_rect.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
+        DISPLAYSURF.blit(text_surf,text_rect)
+        text_surf = BASICFONT.render("To restart, press enter!",True,GREEN)
+        text_rect = text_surf.get_rect()
+        text_rect.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2+30)
+        DISPLAYSURF.blit(text_surf,text_rect)
+        text_surf = BASICFONT.render("Score:"+str(SCORE),True,GREEN)
+        text_rect = text_surf.get_rect()
+        text_rect.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2+60)
+        DISPLAYSURF.blit(text_surf,text_rect)
+        SCORE = 0
+
+    pygame.display.update()
+
 class word_on_screen:
 
     def __init__(self, word):
         global name, position
         self.name = word
-        self.position = [0,random.randint(20,WINDOWHEIGHT/10*9.0)]
+        self.position = [0,random.randint(20,WINDOWHEIGHT/10*9)]
+
 
     def word_move(self):
         self.position[0] += WINDOWWIDTH / 64
 
     def word_draw(self):
+        global LIFECOUNT
+        color = [GREEN, YELLOW, RED]
         text_surf = BASICFONT.render(self.name,True,WHITE)
         text_rect = text_surf.get_rect()
         delete_rect = text_surf.get_rect()
@@ -141,7 +205,17 @@ class word_on_screen:
         delete_rect.bottomleft = tuple(self.position)
         delete_rect.left -= WINDOWWIDTH / 64
         pygame.draw.rect(DISPLAYSURF, BGCOLOR, delete_rect)
+        if text_rect.center[0] < 1/3 * WINDOWWIDTH:
+            text_surf = BASICFONT.render(self.name,True,color[0])
+        if text_rect.center[0] > 1/3 * WINDOWWIDTH:
+            text_surf = BASICFONT.render(self.name,True,color[1])
+        if text_rect.center[0] > 2/3 * WINDOWWIDTH:
+            text_surf = BASICFONT.render(self.name,True,color[2])
         DISPLAYSURF.blit(text_surf,text_rect)
+        if text_rect.right >= WINDOWWIDTH:
+            self.word_remove_lose_life()
+            if LIFECOUNT == 0:
+                game_over()
 
     def word_remove(self):
         WORDS_ON_SCREEN.remove(self)
@@ -149,6 +223,21 @@ class word_on_screen:
         delete_rect = text_surf.get_rect()
         delete_rect.bottomleft = tuple(self.position)
         pygame.draw.rect(DISPLAYSURF, BGCOLOR, delete_rect)
+
+    def word_remove_lose_life(self):
+        global LIFECOUNT
+        WORDS_ON_SCREEN.remove(self)
+        text_surf = BASICFONT.render(self.name,True,WHITE)
+        delete_rect = text_surf.get_rect()
+        delete_rect.bottomleft = tuple(self.position)
+        pygame.draw.rect(DISPLAYSURF, BGCOLOR, delete_rect)
+        LIFECOUNT-=1
+
+
+    def check_for_position(self):
+        pass
+
+
 
 if __name__ == '__main__':
     main()
